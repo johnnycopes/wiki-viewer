d9f25dc83fb45ac5af81be728c479c4b9dfbbb4f
John Coppola <jcoppola@instien.com>
2017-10-20 13:59:52 -0400

    basic search layout under construction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const searchBtn = document.querySelector('.search-icon');
+const searchModal = document.querySelector('.search-modal');
+const searchBar = document.querySelector('.search-bar');
+const closeBtn = document.querySelector('.close-icon');
+
+searchBtn.addEventListener('click', function() {
+  searchBtn.classList.add('fade-out');
+  searchModal.classList.add('slide-down');
+  searchBar.classList.add('fade-in');
+  searchBar.focus();
+});
+
+closeBtn.addEventListener('click', function() {
+  searchModal.classList.remove('slide-down');
+  searchBar.classList.remove('fade-in');
+  searchBtn.classList.remove('fade-out');
+});
+
+
+
+
 let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=butt";
 
 fetch(url)
