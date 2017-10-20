48934b1f75566d1a3a5122f3673e679a76c41e9a
John Coppola <jcoppola@instien.com>
2017-10-20 17:06:31 -0400

    search with enter key in place (prints results in console)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('header');
 const searchBtn = document.querySelector('.search-icon');
 const searchModal = document.querySelector('.search-modal');
 const searchBar = document.querySelector('.search-bar');
@@ -16,22 +17,25 @@ closeBtn.addEventListener('click', function() {
   searchBtn.classList.remove('fade-out');
 });
 
+searchBar.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13) {
+    let query = searchBar.value;
+    let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=50&generator=search&origin=*&gsrsearch=" + query;
 
-
-
-let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=butt";
-
-fetch(url)
-  .then(function(response) {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Network response was not ok: " + response.statusText);
-  })
-  .then(function(data) {
-		// do something with data
-		console.log('got it', data);
-	})
-	.catch(function(error) {
-		console.log(error);
-	});
+    fetch(url)
+      .then(function(response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok: " + response.statusText);
+      })
+      .then(function(data) {
+        // do something with data
+        let results = data.query.pages;
+        console.log(results);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+});
