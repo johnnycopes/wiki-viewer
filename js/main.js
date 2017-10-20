318ebb318d9c0b00d97b5deeecf6a4fed6eff062
John Coppola <jcoppola@instien.com>
2017-10-20 09:47:48 -0400

    deletes axios dependency (uses fetch instead) and achieves basic wiki API call

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-console.log('hey');
-console.log(axios);
+let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=butt";
+
+fetch(url)
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error("Network response was not ok: " + response.statusText);
+  })
+  .then(function(data) {
+		// do something with data
+		console.log('got it', data);
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
